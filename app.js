f6b42c0b324baa65692b33551e2b85055c2138f6
daveball <dave.mike.ball@gmail.com>
2018-01-31 10:18:03 +0000

    Added Api , Config  and models

--- app.js
+++ app.js
@@ -4,11 +4,35 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var config = require('./config/database');
 
+
+try{
+    mongoose.connect(config.database);
+}
+catch(e){
+    console.log(e.message);
+}
+
+
+//add route for our api
+var api = require('./routes/api');
+
+//Comment out the  routes we are not using
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// add Cors Support before any routing
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(passport.initialize());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +46,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+//app.use('/', index);
+app.use('/api', api);
 app.use('/users', users);
+
+app.get('/', function(req, res) {
+    res.send('Page under construction.');
+});
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
